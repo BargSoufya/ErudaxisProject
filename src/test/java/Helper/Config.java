9000ec7c5ac6b1b6ed3de606a04736b0959fb258
BargSoufya <barg.aicha89@gmail.com>
2026-06-08 11:43:04 +0200

    Update Config.java

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -1,18 +1,29 @@
 package Helper;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Config {
 
-	public static  WebDriver driver;
-	public static void Maximaize ( ) {
-		driver.manage().window().maximize();
-	}
-	
-	public static void attente ( int s ) {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
-	}
-}
+    public static WebDriver driver;
 
+    public static void initDriver() {
+        ChromeOptions options = new ChromeOptions();
+
+        if (System.getenv("GITHUB_ACTIONS") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    public static void attente(int s) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
+    }
+}
