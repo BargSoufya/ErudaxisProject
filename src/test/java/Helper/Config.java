44f4a88a00c9a7c97b36f0c4b65c1a3079e27676
BargSoufya <barg.aicha89@gmail.com>
2026-06-08 11:47:48 +0200

    Update Config.java

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -8,20 +8,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class Config {
 
     public static WebDriver driver;
+       public static void maximazwindow() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--window-size=1920,1080");
+    options.addArguments("--remote-allow-origins=*");
+    options.addArguments("--disable-extensions");
+    options.addArguments("--disable-software-rasterizer");
 
-    public static void initDriver() {
-        ChromeOptions options = new ChromeOptions();
-
-        if (System.getenv("GITHUB_ACTIONS") != null) {
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--window-size=1920,1080");
-        }
-
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-    }
+    driver = new ChromeDriver(options);  // ← driver created HERE with all flags
+}
 
     public static void attente(int s) {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
