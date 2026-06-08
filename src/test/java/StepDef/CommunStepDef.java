50a4757e7c9e4c96c673bcffcee981b28e3306b0
BargSoufya <barg.aicha89@gmail.com>
2026-06-08 12:04:16 +0200

    Update CommunStepDef.java

--- src/test/java/StepDef/CommunStepDef.java
+++ src/test/java/StepDef/CommunStepDef.java
@@ -10,7 +10,7 @@ import io.cucumber.java.en.Given;
 public class CommunStepDef {
 	@Given("l admin est authentifie par  le bon {string} et le bon password {string} And  choisit le role {string}")
 	public void l_admin_est_authentifie_par_le_bon_et_le_bon_password_and_choisit_le_role(String email_user, String pwd, String role) {
-		 Config.driver= new ChromeDriver();
+		 //Config.driver= new ChromeDriver();
 		 Config.Maximaize();
 		 String url = "https://staging.erudaxis.com/dashboards/default";
 		 Config.driver.get(url);
