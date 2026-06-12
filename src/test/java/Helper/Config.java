2dee8f3270cb4532e0b743a0e03b9fd5b375d841
BargSoufya <barg.aicha89@gmail.com>
2026-06-12 20:22:49 +0200

    Update Config.java

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -38,12 +38,21 @@ public class Config {
 //     options.addArguments("--disable-software-rasterizer");
 
 //     driver = new ChromeDriver(options);  // ← driver created HERE with all flags
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--headless=new");
+    // ChromeOptions options = new ChromeOptions();
+    // options.addArguments("--no-sandbox");
+    // options.addArguments("--disable-dev-shm-usage");
+    // options.addArguments("--headless=new");
+         options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-software-rasterizer");
         driver = new ChromeDriver(options);
-        
+                driver.manage().window().maximize();
+
         
     }
 
