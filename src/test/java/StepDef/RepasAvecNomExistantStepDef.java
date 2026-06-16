bc586df103cb951cf3087713983a06abde6273c6
BargSoufya <barg.aicha89@gmail.com>
2026-06-16 19:29:35 +0200

    Update RepasAvecNomExistantStepDef.java

--- src/test/java/StepDef/RepasAvecNomExistantStepDef.java
+++ src/test/java/StepDef/RepasAvecNomExistantStepDef.java
@@ -20,7 +20,7 @@ public class RepasAvecNomExistantStepDef {
 @Given("l admin est authentifié par l'email {string} et le mot de passe {string}")
 public void l_admin_est_authentifié_par_l_email_et_le_mot_de_passe(String email, String pwd) {
 	//Config.driver = new ChromeDriver ();
-Config.Maximaize();
+Config.Maximize();
    String url="https://staging.erudaxis.com/";
    Config.driver.get(url);
    LoginPage logp=new LoginPage();
