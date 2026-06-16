753a6a8fa3ba504a8a77c7c23de88648fed86df4
BargSoufya <barg.aicha89@gmail.com>
2026-06-16 19:35:49 +0200

    Update AjouterUnNouveauBusPage.java

--- src/test/java/Pages/AjouterUnNouveauBusPage.java
+++ src/test/java/Pages/AjouterUnNouveauBusPage.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.TimeoutException;
+
 
 import Helper.Config;
 
