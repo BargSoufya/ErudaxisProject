ef12866bd82ade8acb47300f0ef30c7170d8d19a
BargSoufya <barg.aicha89@gmail.com>
2026-06-16 20:31:03 +0200

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,24 +1,21 @@
 package Runner;
+
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-<<<<<<< HEAD
-    features = "src/main/resources/Features/AjouterReapsAvecNomVide.feature",
-=======
     features = "src/main/resources/Features/",
-	tags = "",
->>>>>>> 753a6a8fa3ba504a8a77c7c23de88648fed86df4
+    tags = "",
     glue = "StepDef",
     plugin = {
         "pretty",
         "html:target/cucumber/report.html",
-        "json:target/cucumber/report.json",         // ✅ Pour Cucumber HTML Report
-        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"  // ✅ Pour Allure
+        "json:target/cucumber/report.json",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
     }
 )
 public class TestRunner {
-	
+    
 }
