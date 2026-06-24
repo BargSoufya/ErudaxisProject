c0a5d2d1874f20a801cba1af5a9026f0693afb60
BargSoufya <barg.aicha89@gmail.com>
2026-06-24 19:23:30 +0200

    Update Config.java

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -9,51 +9,22 @@ public class Config {
 
     public static WebDriver driver;
 
-    // public static void Maximaize() {
-    //     ChromeOptions options = new ChromeOptions();
+    public static void Maximaize() {
+        driver.manage().window().maximize();
+    }
 
-    //     // Always apply these on CI (GitHub Actions has no display)
-    //     options.addArguments("--headless=new");
-    //     options.addArguments("--no-sandbox");
-    //     options.addArguments("--disable-dev-shm-usage");
-    //     options.addArguments("--disable-gpu");
-    //     options.addArguments("--window-size=1920,1080");
-    //     options.addArguments("--remote-allow-origins=*");
-    //     options.addArguments("--disable-extensions");
-    //     options.addArguments("--disable-software-rasterizer");
-
-    //     driver = new ChromeDriver(options);
-    //     driver.manage().window().maximize();
-    // }
-
-    public static void Maximize() {
-  ChromeOptions options = new ChromeOptions();
-//     options.addArguments("--headless=new");
-//     options.addArguments("--no-sandbox");
-//     options.addArguments("--disable-dev-shm-usage");
-//     options.addArguments("--disable-gpu");
-//     options.addArguments("--window-size=1920,1080");
-//     options.addArguments("--remote-allow-origins=*");
-//     options.addArguments("--disable-extensions");
-//     options.addArguments("--disable-software-rasterizer");
-
-//     driver = new ChromeDriver(options);  // ← driver created HERE with all flags
-    // ChromeOptions options = new ChromeOptions();
-    // options.addArguments("--no-sandbox");
-    // options.addArguments("--disable-dev-shm-usage");
-    // options.addArguments("--headless=new");
-         options.addArguments("--headless=new");
+    public static void initialiserDriver() {
+        ChromeOptions options = new ChromeOptions();
+        
+        // ✅ OBLIGATOIRE pour GitHub Actions (pas d'écran physique)
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-software-rasterizer");
-        driver = new ChromeDriver(options);
-                driver.manage().window().maximize();
-
         
+        driver = new ChromeDriver(options);
     }
 
     public static void attente(int s) {
